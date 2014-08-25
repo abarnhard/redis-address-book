72087d4d11e5b9ece4aea12cf5d78c20eafadccd
Adam Barnhard <admbarn@gmail.com>
2014-08-25 13:57:56 -0500

    added authentication

---
+++ app/lib/security.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var User = require('../models/user');
+
+exports.authenticate = function(req, res, next){
+  if(!req.session.userId){return next();}
+
+  User.findById(req.session.userId, function(err, user){
+    res.locals.user = user;
+    next();
+  });
+
+};
+
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -4,6 +4,10 @@ var morgan         = require('morgan'),
     bodyParser     = require('body-parser'),
     methodOverride = require('express-method-override'),
     less           = require('less-middleware'),
+    session        = require('express-session'),
+    RedisStore     = require('connect-redis')(session),
+    security       = require('../lib/security'),
+    users          = require('../controllers/users'),
     home           = require('../controllers/home');
 
 module.exports = function(app, express){
@@ -12,8 +16,19 @@ module.exports = function(app, express){
   app.use(express.static(__dirname + '/../static'));
   app.use(bodyParser.urlencoded({extended:true}));
   app.use(methodOverride());
+  // maxAge is in seconds (3600:1hr, 86400:1d), null makes cookie never expire
+  app.use(session({store: new RedisStore(), secret:'so safe wow', resave:true, saveUninitialized:true, cookie:{maxAge:null}}));
+
+  // authentication step
+  app.use(security.authenticate);
 
   app.get('/', home.index);
+
+  app.get('/register', users.new);
+  app.post('/register', users.create);
+  app.get('/login', users.login);
+  app.post('/login', users.authenticate);
+  app.delete('/logout', users.logout);
 
   console.log('Express: Routes Loaded');
 };
